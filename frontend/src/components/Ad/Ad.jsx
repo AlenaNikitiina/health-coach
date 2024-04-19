622e139c7b2ab636ee4b3b81c14c9fd86dfad8ca
Alena Nikitina <alena.tkachenk@mail.ru>
2024-04-19 22:15:20 +0300

    add new files in cache

--- frontend/src/components/Ad/Ad.jsx
+++
@@ -1,8 +0,0 @@
-import '../Ad/Ad.css';
-import food from '../../images/food.jpg';
-
-export default function Ad () {
-  return (
-    <img className='eda' src={food} alt="" />
-  )
-}
--- frontend/src/components/Diploma/Diploma.jsx
+++ frontend/src/components/Diploma/Diploma.jsx
@@ -3,12 +3,18 @@ import lemons from '../../images/lemons.jpg';
 
 export default function Diploma () {
   return (
-    <section className='diploma popup popup_type_images splide' id="splide_place_gallery-popup" aria-label="всплывающее окно с картинкой">
-      <div className='diploma__content popup__container-images'>
+    <section className='diploma' aria-label="всплывающее окно с картинкой">
+      <div className='diploma__content'>
         <h2 className='diploma__heading'>Мои дипломы</h2>
-        <ul className='diploma__docs link'>
+        <ul className='diploma__list link'>
           <li>
-            <img className='diploma__photo' src={lemons} alt="медицинсоке образование" />
+            <img className='diploma__photo' src={lemons} alt="диплом о медицинском образовании" />
+          </li>
+          <li>
+            <img className='diploma__photo'src={lemons} alt="диплом об образовании психолога" />
+          </li>
+          <li>
+            <img className='diploma__photo'src={lemons} alt="диплом о завершении ординатуры" />
           </li>
           <li>
             <img className='diploma__photo'src={lemons} alt="курс нутрициолога" />
