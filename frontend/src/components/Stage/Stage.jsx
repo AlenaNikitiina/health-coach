a72f7d9396916934e5b1d3b1a2b09ed2520b2bfb
Alena Nikitina <alena.tkachenk@mail.ru>
2024-04-19 14:54:17 +0300

    fix: add adaptation in stage

--- frontend/src/components/Stage/Stage.jsx
+++ frontend/src/components/Stage/Stage.jsx
@@ -10,20 +10,21 @@ export default function Stage() {
       <div className='stage__content'>
 
         <article className='stage__text'>
-          <img className='stage__imgf' src={salad} alt='мое фото' />
-          <div className='stage__listf'>
+          <img className='stage__imgfirst' src={salad} alt='мое фото' />
+          <div className='stage__listfirst'>
             <h2 className='stage__subtitle'>(1)&nbsp;Знакомство</h2>
             <p className='stage__paragraph'>Вы заполняете анкету, чтобы я могла лучше вас узнать. Если ваш запрос входит в мою компетенцию, то мы договариваемся о дате и времени первичной консультации. Первичная консультация-20 минут, она бесплатная. Время для того, чтобы познакомиться и понять подходим ли мы друг другу. Я тезисно рассказываю о моей работе исходя из ваших запросов и если мы понимаем, что нас всё устраивает, то работаем дальше и выбираем дальнейший путь.</p>
           </div>
         </article>
 
         <article className='stage__text'>
-          <img className='stage__imgt' src={lemons} alt='мое фото' />
-          <div className='stage__listt'>
+          <img className='stage__imgsecond' src={lemons} alt='мое фото' />
+          <div className='stage__listsecond'>
             <h2 className='stage__subtitle'>(2)&nbsp;Анализ вашего состояния</h2>
             <p className='stage__paragraph'>Совместно мы проведем анализ вашего текущего состояния и в зависимости от ваших запросов мы выбираем 3 пути: -консультативная помощь.-групповое ведение.-индивидуальное сопровождение.Я не даю консультации тем, у кого имеются тяжелые и среднетяжелые формы расстройства пищевого поведения. Это вопрос, который требует специализированной помощи врача психиатра и стационарного лечения. Я не работаю с беременными, детьми, женщинами на грудном вскармливании и профессиональными спортсменами.</p>
           </div>
         </article>
+      
       </div>
     </section>
   )
