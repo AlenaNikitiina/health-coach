1887f456940b61017f12763c9c030c0996cebf53
Alena Nikitina <alena.tkachenk@mail.ru>
2024-04-21 09:23:00 +0300

    fix: add photo of diplomas and try popup

--- frontend/src/components/Diploma/Diploma.jsx
+++ frontend/src/components/Diploma/Diploma.jsx
@@ -1,29 +1,42 @@
 import './Diploma.css';
-import lemons from '../../images/lemons.jpg';
+//import lemons from '../../images/lemons.jpg';
+
+import coach from '../../images/coach.jpg';
+import psyho from '../../images/psyho.jpg';
+import doctor from '../../images/doctor.jpg';
+import moniki from '../../images/moniki.jpg';
+import nutri from '../../images/nutri.jpg';
 
 export default function Diploma () {
   return (
     <section className='diploma' aria-label="всплывающее окно с картинкой">
       <div className='diploma__content'>
         <h2 className='diploma__heading'>Мои дипломы</h2>
+
         <ul className='diploma__list link'>
-          <li>
-            <img className='diploma__photo' src={lemons} alt="диплом о медицинском образовании" />
+          <li className='diploma__img'>
+            <img className='diploma__photo' src={doctor} alt="диплом о медицинском образовании" />
           </li>
-          <li>
-            <img className='diploma__photo'src={lemons} alt="диплом об образовании психолога" />
+          <li className='diploma__img'>
+            <img className='diploma__photo'src={psyho} alt="диплом об образовании психолога" />
           </li>
-          <li>
-            <img className='diploma__photo'src={lemons} alt="диплом о завершении ординатуры" />
+          <li className='diploma__img'>
+            <img className='diploma__photo'src={moniki} alt="диплом о завершении ординатуры" />
           </li>
-          <li>
-            <img className='diploma__photo'src={lemons} alt="курс нутрициолога" />
+          <li className='diploma__img'>
+            <img className='diploma__photo'src={coach} alt="курс ментора" />
           </li>
-          <li>
-            <img className='diploma__photo'src={lemons} alt="курс нутрициолога" />
+          <li className='diploma__img'>
+            <img className='diploma__photo'src={nutri} alt="курс нутрициолога" />
           </li>
         </ul>
       </div>
+
+      <div class='popup'>
+      <span>&times;</span>
+      <img src='nutri.jpg' alt="" />
+    </div>
+    
     </section>
   )
 };
--- frontend/src/components/Warning/Warning.jsx
+++ frontend/src/components/Warning/Warning.jsx
@@ -7,13 +7,14 @@ export default function Warning () {
       <div className='warning__content'>
         <article className='warning__text'>
           <img className='warning__img' src={lenta} alt='measuring tape' />
-          <h2 className='warning__title'>С кем я</h2>
+          <h2 className='warning__title'>Напутствие</h2>
           <div>
             
             <p className='warning__paragraph'>Хочу отметить, что иногда требуется совместная работа с другими специалистами и врачами. Если я рекомендую вам консультацию у терапевта, эндокринолога или психиатра, настоятельно рекомендую вам следовать этой рекомендации. Только совместные усилия могут привести к наилучшим результатам.</p>
+            <p className='warning__paragraph'>Достижение цели, какой бы она не была долговременной и тяжелой - это ряд ступенек. Больших и маленьких шагов. А моя задача сделать этот первый шаг реальным. И он будет вместе со мной.</p>
           </div>
         </article>
       </div>
     </section>
   )
-}
+};
