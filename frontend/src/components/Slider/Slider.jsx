d2cebad19f377a9fb1ce6f499416ee2f773a7fb1
Alena Nikitina <alena.tkachenk@mail.ru>
2024-05-07 23:13:58 +0300

    fix set up slider

--- frontend/src/components/Slider/Slider.jsx
+++ frontend/src/components/Slider/Slider.jsx
@@ -12,61 +12,61 @@ import reviewT from '../../images/reviewT.jpg';
 export default function Slider () {
 
   return (
-    <div class="container">
-      <div className='slider'>
+    <section id='reviews' class="slider">
+      <div className='slider__content'>
+        <h2 className='slider__heading'>Отзывы</h2>
+
         <div className='slider__navigation'>
-          <button className='slider__btn btnp slider__btn-prev'></button>
+          <button className='slider__btn btnprev slider__btn-prev'></button>
           <div class="slider__pagination"></div>
-          <button className='slider__btn btnn slider__btn-next'></button>
+          <button className='slider__btn btnnext slider__btn-next'></button>
         </div>
 
         <div class="swiper slider-swiper">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide">
-            <a data-fslightbox="slider" className='slider__link' href='image/reviewO.jpg'>
-              <img className='slider__img' src={reviewO} alt="1" />
-            </a>
-          </div>
-          <div class="swiper-slide">
-            <a data-fslightbox="slider" className='slider__link' href="image/reviewOne.jpg">
-              <img className='slider__img' src={reviewOne} alt="2" />
-            </a>
-          </div>
-          <div class="swiper-slide">
-            <a data-fslightbox="slider" className='slider__link' href="images/reviewOn.jpg">
-              <img className='slider__img' src={reviewOn} alt="4" />
-            </a>
-          </div>
-          <div class="swiper-slide">
-            <a data-fslightbox="slider" className='slider__link' href="./images/review.jpg">
-              <img className='slider__img' src={review} alt="3" />
-            </a>
-          </div>
-          <div class="swiper-slide">
-            <a data-fslightbox="slider" className='slider__link' href="images/reviewOnee.jpg">
-              <img className='slider__img' src={reviewOnee} alt="3" />
-            </a>
-          </div>
-          <div class="swiper-slide">
-            <a data-fslightbox="slider" className='slider__link' href="images/reviewtwo.jpg">
-            <img className='slider__img' src={reviewtwo} alt="4" />
-            </a>
-          </div>
-          <div class="swiper-slide">
-            <a data-fslightbox="slider" className='slider__link' href="images/reviewTw.jpg">
-              <img className='slider__img' src={reviewTw} alt="1" />
-            </a>
-          </div>
-
-          <div class="swiper-slide">
-            <a data-fslightbox="slider" className='slider__link' href="images/reviewT.jpg">
-              <img className='slider__img' src={reviewT} alt="1" />
-            </a>
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <a data-fslightbox="slider" className='slider__link' href='image/reviewO.jpg'>
+                <img className='slider__img' src={reviewO} alt="1" />
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a data-fslightbox="slider" className='slider__link' href="image/reviewOne.jpg">
+                <img className='slider__img' src={reviewOne} alt="2" />
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a data-fslightbox="slider" className='slider__link' href="images/reviewOn.jpg">
+                <img className='slider__img' src={reviewOn} alt="4" />
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a data-fslightbox="slider" className='slider__link' href="./images/review.jpg">
+                <img className='slider__img' src={review} alt="3" />
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a data-fslightbox="slider" className='slider__link' href="images/reviewOnee.jpg">
+                <img className='slider__img' src={reviewOnee} alt="3" />
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a data-fslightbox="slider" className='slider__link' href="images/reviewtwo.jpg">
+              <img className='slider__img' src={reviewtwo} alt="4" />
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a data-fslightbox="slider" className='slider__link' href="images/reviewTw.jpg">
+                <img className='slider__img' src={reviewTw} alt="1" />
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a data-fslightbox="slider" className='slider__link' href="images/reviewT.jpg">
+                <img className='slider__img' src={reviewT} alt="1" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
-      </div>
-
-    </div>
+    </section>
   );
 }
