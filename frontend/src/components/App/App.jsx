5c1e61c8d43be7cc18ce26f8ded2331d182ca668
Alena Nikitina <alena.tkachenk@mail.ru>
2024-05-09 12:59:07 +0300

    put the components together in main

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
 import './App.css';
-import Header from '../Header/Header';
-import Promo from '../Promo/Promo';
-import AboutMe from '../AboutMe/AboutMe';
-import Footer from '../Footer/Footer';
-import Add from '../Add/Add';
-import Stage from '../Stage/Stage';
-import Warning from '../Warning/Warning';
-import Consultation from '../Consultation/Consultation';
-import Diploma from '../Diploma/Diploma';
-import Quote from '../Quote/Quote';
-import Gallery from '../Gallery/Gallery.jsx';
-import Slider from '../Slider/Slider.js';
+import { useState } from "react";
+import ImagePopup from '../ImagePopup/ImagePopup.js';
+import { Route, Routes } from 'react-router-dom';
 
+import Header from '../Header/Header';
+import Main from '../Main/Main.jsx';
+import Footer from '../Footer/Footer';
+import Head from '../Head/Head.jsx';
 import MyStory from '../MyStory/MyStory';
 import MyStoryA from '../MyStoryA/MyStoryA.jsx';
 import MyStoryB from '../MyStoryB/MyStoryB.jsx';
 import MyStoryC from '../MyStoryC/MyStoryC.jsx';
-import Head from '../Head/Head.jsx';
-
-import { useState } from "react";
-import ImagePopup from '../ImagePopup/ImagePopup.js';
-import { Route, Routes } from 'react-router-dom';
+import Gallery from '../Gallery/Gallery.jsx';
 
 export default function App() {
   const [selectedCard, setSelectedCard] = useState (null);  // zoom при клике на фото
@@ -47,35 +38,10 @@ export default function App() {
   return (
     <div className='app'>
       <Routes>
-
-        <Route path='/mystory' element={
-          <>
-            <Head />
-            <MyStory />
-            <MyStoryA />
-            <MyStoryB />
-            <MyStoryC />
-            <Gallery />
-            <Footer />
-          </>
-          }>
-        </Route>
-    
         <Route path='/' element={
           <>
             <Header />
-            <Promo />
-            <Quote />
-            <AboutMe />
-            <Diploma
-              onCardClick={handleCardClick} // zoom f
-              cards = {cards}
-            />
-            <Add />
-            <Stage />
-            <Consultation />
-            <Slider/>
-            <Warning />
+            <Main />
             <Footer />
 
             <ImagePopup
@@ -89,6 +55,18 @@ export default function App() {
           }>
         </Route>
 
+        <Route path='/mystory' element={
+          <>
+            <Head />
+            <MyStory />
+            <MyStoryA />
+            <MyStoryB />
+            <MyStoryC />
+            <Gallery />
+            <Footer />
+          </>
+          }>
+        </Route>
       </Routes>
     </div>
   )
---
+++ frontend/src/components/Main/Main.jsx
@@ -0,0 +1,30 @@
+import Promo from "../Promo/Promo";
+import Quote from "../Quote/Quote";
+import AboutMe from "../AboutMe/AboutMe";
+import Diploma from "../Diploma/Diploma";
+import Add from "../Add/Add";
+import Stage from "../Stage/Stage";
+import Consultation from "../Consultation/Consultation";
+import Slider from "../Slider/Slider";
+import Warning from "../Warning/Warning";
+
+export default function Main() {
+  return (
+    <main>
+      <Promo />
+      <Quote />
+      <AboutMe />
+      <Diploma />
+      <Add />
+      <Stage />
+      <Consultation />
+      <Slider />
+      <Warning />
+    </main>
+  )
+}
+
+/*<Diploma
+        onCardClick={handleCardClick} // zoom f
+        cards = {cards}
+      />*/
