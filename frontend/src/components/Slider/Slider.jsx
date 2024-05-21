1c08100ecdcc1b953f5fd72e2fc0ed19d40d2e3b
Alena Nikitina <alena.tkachenk@mail.ru>
2024-05-21 13:46:45 +0300

    style in slider. finish

--- frontend/src/components/Slider/Slider.jsx
+++ frontend/src/components/Slider/Slider.jsx
@@ -1,86 +1,9 @@
-/*import "./Slider.css";
-
-import videoreview from "../../images/review/videoreview.mp4";
-import videoreview2 from "../../images/review/videoreview2.mp4";
-import reviewOne from "../../images/review/reviewOne.jpg";
-import reviewOn from "../../images/review/reviewOn.jpg";
-import reviewOnee from "../../images/review/reviewOnee.jpg";
-import reviewT from "../../images/review/reviewT.jpg";
-import common from "../../images/review/common.jpg";
-import alina from "../../images/review/alina.jpg";
-import ali from "../../images/review/ali.jpg";
-import mari from "../../images/review/mari.jpg";
-import rew from "../../images/review/rew.jpg";
-
-
-export default function Slider () {
-
-  return (
-    <section id="reviews" className="slider">
-      <div className="slider__content">
-        <h2 className="slider__heading">Отзывы</h2>
-
-        <div className="slider__navigation">
-          <button className="slider__btn btnprev slider__btn-prev"></button>
-          <div className="slider__pagination"></div>
-          <button className="slider__btn btnnext slider__btn-next"></button>
-        </div>
-
-        <div className="swiper slider-swiper">
-          <div className="swiper-wrapper">
-
-            <div className="swiper-slide">
-              <img className="slider__img" src={mari} alt="1" />
-            </div>
-            <div className="swiper-slide">
-              <img className="slider__img" src={reviewOne} alt="1" />
-            </div>
-            <div className="swiper-slide">
-              <img className="slider__img" src={reviewOn} alt="1" />
-            </div>
-            <div className="swiper-slide">
-              <img className="slider__img" src={reviewOnee} alt="1" />
-            </div>
-
-           
-
-            <div className="swiper-slide">
-              <img className="slider__img" src={reviewT} alt="2" />
-            </div>
-            <div className="swiper-slide">
-              <img className="slider__img" src={common} alt="3" />
-            </div>
-            <div className="swiper-slide">
-              <img className="slider__img" src={alina} alt="4" />
-            </div>
-            <div className="swiper-slide">
-              <img className="slider__img" src={ali} alt="4" />
-            </div>
-            <div className="swiper-slide">
-              <img className="slider__img" src={rew} alt="5" />
-            </div>
-            <div className="swiper-slide">
-              <video className="slider__img slider__video" controlsList="nofullscreen" src={videoreview} controls playsInline ></video>
-            </div>
-            <div className="swiper-slide">
-              <video className="slider__img slider__video" controlsList="nofullscreen" src={videoreview2} controls playsInline loading="lazy" preload="metadata"></video>
-            </div>
-          </div>
-        
-        </div>
-      </div>
-    </section>
-  );
-}
-*/
-
-
 import "./Slider.css";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 
 import reviewOne from "../../images/review/reviewOne.jpg";
@@ -95,13 +18,13 @@ import rew from "../../images/review/rew.jpg";
 
 export default function Slider () {
   return (
-    <section id="reviews" className="sli">
-      <div className="container">
+    <section id="reviews" className="slider">
+      <div className="slider__content">
       <h2 className="slider__heading">Отзывы</h2>
 
       <Swiper
         className="swiper_container"
-        effect={'coverflow'}
+        effect={"coverflow"}
         grabCursor={true}
         centeredSlides={true}
         loop={true}
@@ -119,6 +42,9 @@ export default function Slider () {
           425: {
             slidesPerView: 2,
           },
+          375: {
+            slidesPerView: 1,
+          },
         }}
         coverflowEffect={{
           rotate: 0,
@@ -126,10 +52,10 @@ export default function Slider () {
           depth: 100,
           modifier: 2.5,
         }}
-        pagination={{ el: '.swiper-pagination', clickable: true }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
         navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
           clickable: true,
         }}
         modules={[EffectCoverflow, Pagination, Navigation]}
@@ -163,8 +89,6 @@ export default function Slider () {
         <img src={common} alt="slide_image" />
       </SwiperSlide>
 
-     
-
       <div className="slider-controler slider__navigation">
         <button className="swiper-button-prev slider-arrow slider__btn btnprev slider__btn-prev"></button>
         <div className=" swiper-pagination"></div>
@@ -185,8 +109,6 @@ export default function Slider () {
 <video className="slider__img slider__video" controlsList="nofullscreen" src={videoreview2} controls playsInline loading="lazy" preload="metadata"></video>
 </SwiperSlide>
 
-
-
  <div className="slider-controler">
         <div className="swiper-button-prev slider-arrow">
           <ion-icon name="arrow-back-outline"></ion-icon>
@@ -198,4 +120,65 @@ export default function Slider () {
         <div className="swiper-pagination"></div>
       </div>
 
+*/
+
+/*import "./Slider.css";
+
+export default function Slider () {
+
+  return (
+    <section id="reviews" className="slider">
+      <div className="slider__content">
+        <h2 className="slider__heading">Отзывы</h2>
+
+        <div className="slider__navigation">
+          <button className="slider__btn btnprev slider__btn-prev"></button>
+          <div className="slider__pagination"></div>
+          <button className="slider__btn btnnext slider__btn-next"></button>
+        </div>
+
+        <div className="swiper slider-swiper">
+          <div className="swiper-wrapper">
+
+            <div className="swiper-slide">
+              <img className="slider__img" src={mari} alt="1" />
+            </div>
+            <div className="swiper-slide">
+              <img className="slider__img" src={reviewOne} alt="1" />
+            </div>
+            <div className="swiper-slide">
+              <img className="slider__img" src={reviewOn} alt="1" />
+            </div>
+            <div className="swiper-slide">
+              <img className="slider__img" src={reviewOnee} alt="1" />
+            </div>
+           
+            <div className="swiper-slide">
+              <img className="slider__img" src={reviewT} alt="2" />
+            </div>
+            <div className="swiper-slide">
+              <img className="slider__img" src={common} alt="3" />
+            </div>
+            <div className="swiper-slide">
+              <img className="slider__img" src={alina} alt="4" />
+            </div>
+            <div className="swiper-slide">
+              <img className="slider__img" src={ali} alt="4" />
+            </div>
+            <div className="swiper-slide">
+              <img className="slider__img" src={rew} alt="5" />
+            </div>
+            <div className="swiper-slide">
+              <video className="slider__img slider__video" controlsList="nofullscreen" src={videoreview} controls playsInline ></video>
+            </div>
+            <div className="swiper-slide">
+              <video className="slider__img slider__video" controlsList="nofullscreen" src={videoreview2} controls playsInline loading="lazy" preload="metadata"></video>
+            </div>
+          </div>
+        
+        </div>
+      </div>
+    </section>
+  );
+}
 */
